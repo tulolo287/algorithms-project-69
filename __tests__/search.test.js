713e836b8a5bd8b5ab7d770ff5edee65292812ba
tulolo287 <iv1055@yandex.ru>
2024-09-16 23:02:09 +0300

    add test multiple words

--- __tests__/search.test.js
+++ __tests__/search.test.js
@@ -42,3 +42,19 @@ test("search word in string with signs and relevant", () => {
 
   expect(search(docs, word)).toEqual(["doc2", "doc1"]);
 });
+
+test("search word in string with signs relevant and multiple words", () => {
+  const doc1 = {
+    id: "doc1",
+    text: "I can't shoot straight unless I've had a pint!",
+  };
+  const doc2 = {
+    id: "doc2",
+    text: "Don't shoot shoot shoot that thing at me.",
+  };
+  const doc3 = { id: "doc3", text: "I'm your shooter." };
+  const docs = [doc1, doc2, doc3];
+  const word = "shoot at me";
+
+  expect(search(docs, word)).toEqual(["doc2", "doc1"]);
+});
