a9a88bfa17611b50d8157a2a8f3357430aea8e00
tulolo287 <95403916+tulolo287@users.noreply.github.com>
2024-10-07 11:14:34 +0000

    add reverse index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 export default function search(docs, word) {
-  let result = [];
-  let term = word.match(/\w+/);
-  let reg = new RegExp(`\\b${term}\\b`, "g");
-  let counter = 1;
-
-  for (let doc of docs) {
-    let wordsArr = doc.text.match(reg);
-    if (wordsArr && wordsArr.length > counter) {
-      result.unshift(doc.id);
-      counter = wordsArr.length;
-    } else if (wordsArr) result.push(doc.id);
+  const index = reverseIndex(docs);
+  const words = word.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    let term = word.match(/\w+/);
+    if (index[term]) {
+      return index[term];
+    }
   }
-  return result;
+}
+
+function reverseIndex(docs) {
+  let index = {};
+  let counter = {};
+  docs.forEach((item, idx) => {
+    item.text.split(" ").map((word) => {
+      let term = word.match(/\w+/);
+      if (!index[term]) index[term] = [];
+      if (!index[term].includes(item.id)) {
+        counter[item.id] = 1;
+        index[term].push(item.id);
+      } else {
+        counter[item.id] += 1;
+        if (counter[index[term][0]] < counter[item.id]) {
+          let temp = index[term][0];
+          index[term][0] = item.id;
+          index[term][idx] = temp;
+        }
+      }
+    });
+  });
+  return index;
 }
